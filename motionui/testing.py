51573dcea6135a37dbc333975966b6100c84e268
Vitaliy Agoshkov <fracturer@gmail.com>
2018-05-01 11:47:18 +0500

    Travis config

---
+++ motionui/testing.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'data_db': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data_db.sqlite3'),
+    }
+}
